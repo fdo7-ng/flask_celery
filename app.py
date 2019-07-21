abe94f6b79372ead0d047972d0cdd565af9fc0de
fdo-ng <blizzytemp@hotmail.com>
2019-07-20 23:30:10 -0400

    added field to main page

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'fdo1979@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'ftest@gmail.com'
 
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
